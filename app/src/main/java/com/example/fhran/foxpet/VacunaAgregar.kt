dfe32f8d6dece305a052ff3955b60a194cbc01a1
Luxusz <fhrancisco.rios@gmail.com>
2018-04-18 22:27:17 -0300

    2do Commit

--- app/src/main/java/com/example/fhran/foxpet/VacunaAgregar.kt
+++ app/src/main/java/com/example/fhran/foxpet/VacunaAgregar.kt
@@ -1,12 +1,25 @@
 package com.example.fhran.foxpet
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_vacuna_agregar.*
 
 class VacunaAgregar : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_vacuna_agregar)
+
+        btnAdd.setOnClickListener(){
+
+            Toast.makeText(this, "Hola", Toast.LENGTH_LONG).show()
+
+            val intento = Intent(this,Lobby::class.java)
+            startActivity(intento)
+        }
+
+
     }
 }
